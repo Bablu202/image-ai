3ae7bd69e292a3b3a098c3877157c5b66b174143
aware <aware9758@gmail.com>
2024-03-20 16:08:58 +0100

    20 eve

---
+++ components/shared/MediaUploader.tsx
@@ -0,0 +1,90 @@
+"use client";
+import Image from "next/image";
+import { useToast } from "../ui/use-toast";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+import { dataUrl, getImageSize } from "@/lib/utils";
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
+
+type MediaUploaderProps = {
+  onValueChange: (value: string) => void;
+  setImage: React.Dispatch<any>;
+  publicId: string;
+  image: any;
+  type: string;
+};
+const MediaUploader = ({
+  onValueChange,
+  setImage,
+  publicId,
+  image,
+  type,
+}: MediaUploaderProps) => {
+  const { toast } = useToast();
+  const onUploadSuccessHandler = (result: any) => {
+    setImage((prevState: any) => ({
+      ...prevState,
+      publicId: result?.info?.public_Id,
+      width: result?.info?.width,
+      height: result?.info?.height,
+      secureURL: result?.info?.secure_url,
+    }));
+    onValueChange(result?.info?.public_id);
+    toast({
+      title: "Image got in successfully",
+      description: "1 credit is used form yours",
+      duration: 3000,
+      className: "success-toast",
+    });
+  };
+  const onUploadErrorHandler = () => {
+    toast({
+      title: "unable to upload, some Error",
+      description: "Please try again",
+      duration: 3000,
+      className: "error-toast",
+    });
+  };
+  return (
+    <CldUploadWidget
+      uploadPreset="image-ai"
+      options={{ multiple: false, resourceType: "image" }}
+      onSuccess={onUploadSuccessHandler}
+      onError={onUploadErrorHandler}
+    >
+      {({ open }) => (
+        <div className="flex flex-col gap-4">
+          <h3 className="h3-bold text-dark-600">Original</h3>
+          {publicId ? (
+            <>
+              <div className="cursor-pointer overflow-hidden rounded-[10px]">
+                <CldImage
+                  width={getImageSize(type, image, "width")}
+                  height={getImageSize(type, image, "height")}
+                  src={publicId}
+                  alt="image"
+                  sizes={"(max-width:767px) 100vw, 50vw"}
+                  placeholder={dataUrl as PlaceholderValue}
+                  className="media-uploader_cldImage"
+                />
+              </div>
+            </>
+          ) : (
+            <div className="media-uploader_cta" onClick={() => open()}>
+              <div className="media-uploader_cta-image">
+                <Image
+                  src="/assets/icons/add.svg"
+                  alt="add image"
+                  width={24}
+                  height={24}
+                />
+              </div>
+              <p className="p-14-medium"> Click here to Upload image</p>
+            </div>
+          )}
+        </div>
+      )}
+    </CldUploadWidget>
+  );
+};
+
+export default MediaUploader;
--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -30,6 +30,12 @@ import {
 import { CustomField } from "./CustomField";
 import { useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
+import MediaUploader from "./MediaUploader";
+import TransformedImage from "./TransformedImage";
+import { updateCredits } from "@/lib/actions/user.actions";
+import { getCldImageUrl } from "next-cloudinary";
+import { useRouter } from "next/navigation";
+import { addImage, updateImage } from "@/lib/actions/image.actions";
 export const formSchema = z.object({
   title: z.string(),
   aspectRatio: z.string().optional(),
@@ -52,8 +58,9 @@ const TransformationForm = ({
     useState<Transformations | null>(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isTransforming, setIsTransforming] = useState(false);
-  const [transformingConfig, setTransformingConfig] = useState(config);
+  const [transformationConfig, setTransformationConfig] = useState(config);
   const [isPending, startTransition] = useTransition();
+  const router = useRouter();
 
   const initialValues =
     data && action === "Update"
@@ -71,8 +78,63 @@ const TransformationForm = ({
     defaultValues: initialValues,
   });
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
+    setIsSubmitting(true);
+    if (data || image) {
+      const transformationUrl = getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      });
+      const imageData = {
+        title: values.title,
+        publicId: image?.publicId,
+        transformationType: type,
+        width: image?.width,
+        height: image?.height,
+        config: transformationConfig,
+        secureURL: image?.secureURL,
+        transformationURL: transformationUrl,
+        aspectRatio: values.aspectRatio,
+        prompt: values.prompt,
+        color: values.color,
+      };
+      if (action === "Add") {
+        try {
+          const newImage = await addImage({
+            image: imageData,
+            userId,
+            path: "/",
+          });
+
+          if (newImage) {
+            form.reset();
+            setImage(data);
+            router.push("/transformations/${newImage._id}");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      if (action === "Update") {
+        try {
+          const updatedImage = await updateImage({
+            image: { ...imageData, _id: data._id },
+            userId,
+            path: `/transformations/${data._id}`,
+          });
+
+          if (updatedImage) {
+            router.push("/transformations/${updatedImage._id}");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+    setIsSubmitting(false);
   }
   const onSelectFieldHandler = (
     value: string,
@@ -88,7 +150,6 @@ const TransformationForm = ({
     }));
     setNewTransformation(transformationType.config);
   };
-  //TODO--update credits after transform
   const onInputChangeHandler = (
     fieldName: string,
     value: string,
@@ -106,14 +167,15 @@ const TransformationForm = ({
       return onChangeField(value);
     }, 1000);
   };
+  //TODO--update credits after transform
   const onTransformHandler = async () => {
     setIsTransforming(true);
-    setTransformingConfig(
-      deepMergeObjects(newTransformation, transformingConfig)
+    setTransformationConfig(
+      deepMergeObjects(newTransformation, transformationConfig)
     );
     setNewTransformation(null);
     startTransition(async () => {
-      //await updateCredits(userId, creditFee)
+      await updateCredits(userId, -1);
     });
   };
   return (
@@ -200,6 +262,29 @@ const TransformationForm = ({
             )}
           </div>
         )}
+        <div className="media-uploader-field">
+          <CustomField
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({ field }) => (
+              <MediaUploader
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+              />
+            )}
+          />
+          <TransformedImage
+            image={image}
+            type={type}
+            title={form.getValues().title}
+            isTransforming={isTransforming}
+            transformationConfig={transformationConfig}
+          />
+        </div>
         <div className="flex flex-col gap-4">
           <Button
             type="button"
